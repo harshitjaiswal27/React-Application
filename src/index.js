4bec2708601a1f417b2ac84d808e80e24d21665b
harshitjaiswal27 <jaiswalhk99@gmail.com>
2020-03-20 22:29:47 +0530

    Video List Component getting added (completed uptil 22)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list'
 
 const API_KEY = 'AIzaSyC9eTy8dtGkhitAOSq71ToF_uDUA1iexE8'; 
 
-const App = function(){
-    return (
-        <div>
-            <SearchBar/>
-        </div>
-    );
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = { videos : []};
+
+        YTSearch({ key : API_KEY , term : 'surfboards'}, function(videos){
+            this.setState({ videos});
+        });
+    }
+    render(){
+        return (
+            <div>
+                <SearchBar/>
+                <VideoList videos={this.state.videos}/>
+            </div>
+        );
+    }
 }
 ReactDOM.render(<App/>, document.querySelector('.container'));
