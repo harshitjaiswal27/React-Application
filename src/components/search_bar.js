c05edbe1a1f14e45c1d4ea3c1d9765af9dca336d
harshitjaiswal27 <jaiswalhk99@gmail.com>
2020-03-20 23:09:19 +0530

    Video list item detail added (completed uptil 25)

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class SearchBar extends Component{
     constructor(){
         super();
-        this.state = {term : 'Starting Value'};
+        this.state = {term : ''};
     }
     render(){
         return (
