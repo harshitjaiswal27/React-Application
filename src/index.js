05308eb22a7d76826f666a787bb3a8949d3c0f20
harshitjaiswal27 <jaiswalhk99@gmail.com>
2020-03-20 22:50:49 +0530

    Video List Item added (completed uptil 23)

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-import VideoList from './components/video_list'
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyC9eTy8dtGkhitAOSq71ToF_uDUA1iexE8'; 
 
 class App extends Component {
     constructor(props){
         super(props);
-        this.state = { videos : []};
+        this.state = { videos : [] };
 
-        YTSearch({ key : API_KEY , term : 'surfboards'}, function(videos){
-            this.setState({ videos});
+        YTSearch({ key : API_KEY , term : 'surfboards'}, (videos)=>{
+            this.setState({videos});
         });
     }
     render(){
