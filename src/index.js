9b400522e4f17f90798c1e429e542c8265c784ce
harshitjaiswal27 <jaiswalhk99@gmail.com>
2020-03-21 01:34:59 +0530

    Styling  added (completed uptil 32)

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail'
+import './App.css';
 
 const API_KEY = 'AIzaSyC9eTy8dtGkhitAOSq71ToF_uDUA1iexE8'; 
 
