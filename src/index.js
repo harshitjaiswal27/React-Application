c62a6e5dac1c9c4b01ff1604e591ca33e0f38f93
harshitjaiswal27 <jaiswalhk99@gmail.com>
2020-03-20 20:47:48 +0530

    Search bar added (completed uptil 19)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import SearchBar from './components/search_bar';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const API_KEY = 'AIzaSyC9eTy8dtGkhitAOSq71ToF_uDUA1iexE8'; 
+
+const App = function(){
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    );
+}
+ReactDOM.render(<App/>, document.querySelector('.container'));
